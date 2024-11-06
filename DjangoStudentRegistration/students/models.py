1c1e82298b615732a90794d2e51187d46c59088d
Abhinav Pandey <abhinavpandey@Toothless.local>
2024-11-06 10:32:32 +0530

    Add : Added courses and students

---
+++ DjangoStudentRegistration/students/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from courses.models import Course
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    roll_number = models.CharField(max_length=10)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
